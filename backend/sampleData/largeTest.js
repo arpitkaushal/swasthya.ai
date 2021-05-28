89aa6dd4e8a1b38a77d14da83b03962a2c520e28
Arpit Kaushal <arpit.kaushal@iitgn.ac.in>
2021-05-29 01:25:45 +0530

    the scripts works okay now

--- backend/sampleData/largeTest.js
+++ backend/sampleData/largeTest.js
@@ -2,15 +2,131 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const axios = require("axios");
-const url = `http://localhost:${process.env.PORT}`;
+const dataPath = "G:/Desktop/may/swastya.ai_new/backend/sampleData/largeData/"
 
-axios({
-    method:"post",
-    url : url,
-    data: {
-        username:"u70",
-        password:"hello1"
+var test = "1";
+
+// /* For users
+const setupUsers = async (userStart,userEnd) => {
+    const signupURL = `http://localhost:8081/signup`;
+    var x = "";
+    var i=0;
+    for(i=start; i<=userEnd; i++){
+        await axios({
+            method:"post",
+            url : signupURL,
+            data: {
+                username:"u"+i,
+                password:"hello1"
+            }
+        })
+        .then(data=>{
+            // console.log(data.data);
+            const us = (data.data.username);
+            const usId = (data.data.userId);
+            // x = us+" "+usId+"\n";
+            x = usId+"\n";
+            console.log(x);
+        })
+        .then(fs.writeFile(dataPath+'users'+test+'.txt',x, { flag: 'a+' }, (err) => {if(err) console.log(err)}))
+        .catch(err=>console.log(err.data));
     }
-})
-.then (data=>console.log(data))
-.catch(err=>console.log(err));
+}
+// */
+
+
+// /* For blogs
+const setupBlogs = async (start,totalBlogs,author) => {
+    
+    const blogURL = `http://localhost:8081/blog/new/${author}`;
+    var x = "";
+    var i=0;
+    for(i=start; i<=start+totalBlogs; i++){
+        await axios({
+            method:"post",
+            url : blogURL,
+            data: {
+                title:"b"+i,
+                body:"something"
+            }
+        })
+        .then(data=>{
+            const bl = (data.data.result.title);
+            const blId = (data.data.result._id);
+            // x = bl+" "+blId+"\n";
+            x = blId+"\n";
+            console.log(x);
+            
+        })
+        .then(fs.writeFile(dataPath+'blogs'+test+'.txt',x, { flag: 'a+' }, (err) => {if(err) console.log(err)}))
+        .catch(err=>console.log(err.data));
+    }
+
+}
+// */
+
+
+// /* For Comments
+const fs = require("fs").promises;
+
+
+const setupComments = async (userStart,commPerUser) => {
+    
+    var users=[];
+    var blogs=[];
+    
+    const userText = await fs.readFile(dataPath+'users'+test+'.txt', 'utf-8', (e,d) => {
+        if(e) return console.log(e);
+        return d;
+    });
+    const blogText = await fs.readFile(dataPath+'blogs'+test+'.txt', 'utf-8', (e,d)=>{
+        if(e) return console.log(e);
+        return d;
+    });
+    users = userText.split("\n");
+    blogs = blogText.split("\n");
+    // console.log(users);
+    // console.log(blogs);
+    
+    var commentURL="http://localhost:8081/comment";
+    var x = "",url="";
+    var i=0; j=0, k=0;
+    var J = blogs.length-1, U = users.length-1;
+    var commNum=userStart*commPerUser;
+    for(i=userStart; i<=U; i++){
+        for(var m = 0; m<commPerUser; m++){
+            j = Math.floor((Math.random() * J));    // select the jth blog
+            url = `${commentURL}/${blogs[j]}/${users[i]}/`;
+            // console.log(url);
+            commNum++;
+            await axios({
+                method:"post",
+                url : url,
+                data: {
+                    body:"c"+commNum,
+                }
+            })
+            .then(data=>{
+                // console.log(data);
+                const cm = (data.data.result.body);
+                const cmId = (data.data.result._id);
+                x = cm+" "+cmId+"\n";
+                // console.log(x);
+                if(commNum%10000 === 0) console.log("10k comments posted.")
+            })
+            .then(fs.writeFile(dataPath+'comments'+test+'.txt',x, { flag: 'a+' }, (err) => {if(err) console.log(err)}))
+            .catch(err=>console.log(err.data));
+
+        }
+    }
+}
+
+// */
+
+
+test = "1";
+const userStart = 10001, userEnd = 100000;
+setupUsers (userStart,userEnd);
+// setupBlogs(1,10000,"60b132721ab6065ba4f786a1");
+// setupComments(userStart,10);
+
