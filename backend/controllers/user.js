95ad61fdb6d786c62c9667de711fc5349bcefa90
Arpit Kaushal <arpit.kaushal@iitgn.ac.in>
2021-05-28 10:20:46 +0530

    removed console logs and added max level possible message, the message doesn't work for u1

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -50,56 +50,32 @@ exports.setLevelNum = (req, res, next, levelNum) => {
 };
 
 exports.getLevelFriends = async (req, res) => {
-    /// write bfs here
     const k = req.level;
-    // console.log(
-    //     "Hey, you're inside the function that we'll complete soon and taste glory!"
-    // );
-    // console.log(
-    //     `You're checking level ${k} friends for the user ${req.profile.username} who has ID ${req.profile._id}.`
-    // );
     const q = await new Queue();
     var user = req.profile._id;
     q.push(user);
-    // console.log("you're prolly here 1");
-    // q.push(user);
-    // q.push(user);
-    // console.log("you're prolly here 2");
-    // console.log(`The length of the queue is ${q.length}.`);
-    var visited = [];
-    visited.push(JSON.stringify(user));
-    // console.log(JSON.stringify(user));
-    console.log(visited);
+    var visited = []; visited.push(JSON.stringify(user));
     var currLevel = 0;
-    // var path = 0;
     while (q.length > 0) {
         console.log(`\nCovered ${currLevel} from root.`);
         console.log(`The queue has ${q.length} elements.`);
-        // console.log(visited);
         
         var n = q.length;
         for (var j = 0; j < n; j++) {
             var x = "";
-            if (currLevel % 2) {
-                // currently at Blog Node
+            if (currLevel % 2) {            // currently at a Blog Node
                 x = await Blog.findById(q.pop(), "comments title").exec();
-                console.log(`\nLooking at ${x.title} from the queue. Comments at this blog: `);
-                // if (!visited.includes(x)) {
-                if (visited.indexOf(JSON.stringify(x._id) === -1)) {
+                if (!(visited.includes(JSON.stringify(x._id)))) {
                     visited.push(JSON.stringify(x._id));
-                    // console.log(`Visited ${x.title} .\n`);
+                    console.log(`Visited ${x.title}`);
                 } 
-                // else console.log(`Had already visited ${x.title}`);
             } 
-            else {
+            else {                          // currently at a User Node
                 x = await User.findById(q.pop(), "comments username").exec();
-                console.log(`\nLooking at ${x.username} from the queue. Comments by this user: `);
-                // if (!visited.includes(x)) {
-                if (visited.indexOf(JSON.stringify(x._id)) === -1) {
+                if (!(visited.includes(JSON.stringify(x._id)))) {
                     visited.push(JSON.stringify(x._id));
-                    console.log(`Visited ${x.username} .\n`);
+                    console.log(`Visited ${x.username}`);
                 }
-                // else console.log(`Had already visited ${x.username}`);
             }
             // get X's comments
             for (com of x.comments) {
@@ -108,79 +84,33 @@ exports.getLevelFriends = async (req, res) => {
                     "commentedAt commentedBy body"
                 ).exec();
 
-                if (currLevel % 2) {
-                    // currently at Blog Node
-                    console.log(
-                        `${y.body} was written by ${y.commentedBy} at ${x.title}.`
-                    );
-                    // if (!visited.includes(y.commentedBy)) {
-                    if (visited.indexOf(JSON.stringify(y.commentedBy)) === -1) {
-                        console.log(`Push this User for ${y.body}.`);
+                if (currLevel % 2) {        // currently at Blog Node
+                    if (!(visited.includes(JSON.stringify(y.commentedBy)))) {
                         q.push(y.commentedBy);
-                        // visited.push(y.commentedBy);
                     } 
-                    else console.log(`Did not Push this User for ${y.body}.`);
-                } else {
-                    // currently at a User Node
-                    // console.log(x.comments);
-                    console.log(
-                        `${y.body} was written by ${x.username} at ${y.commentedAt}.`
-                    );
-                    // if (!visited.includes(y.commentedAt)) {
-                    // console.log(
-                    //     "\n\nManually writing the code for this, checking."
-                    // );
-                    // console.log(visited);
-                    // for (id of visited) {
-                    //     console.log(Object.prototype.JSON.stringify.call(id));
-                    //     console.log(Object.prototype.JSON.stringify.call(x._id));
-                    //     if (id === JSON.stringify(x._id))
-                    //         console.log(`${id} matched with ${JSON.stringify(x._id)}.`);
-                    //     else console.log(`${id} did NOT match.`);
-                    // }
-                    if (visited.indexOf(JSON.stringify(y.commentedAt)) === -1) {
+                } else {                    // currently at a User Node
+                    if (!(visited.includes(JSON.stringify(y.commentedAt)))) {
                         q.push(y.commentedAt);
-                        // visited.push(y.commentedAt);
                     }
                 }
             }
 
-            // console.log(`Path ${currLevel} completed.`);
         }
         currLevel++;
         if (currLevel >= 2 * k ) break;
-        // var x = q.pop();
     }
 
-    // var ans = [];
     var ans = new Set;
     while(q.length){
         const x = await User.findById(q.pop(),'username _id').exec();
-        // ans.push(x);
         ans.add(JSON.stringify(x));
     }
-
-    // const d_ans = ([...set].map(item)) => {
-    //     if (typeof item === 'string') return JSON.parse(item);
-    //     else if (typeof item === 'object') return item;
-    // };
     
     const d_ans = [];
-    for( id of ans){
-        if (typeof id === 'string') d_ans.push(JSON.parse(id));
-        else if (typeof id === 'object') d_ans.push(id);
-    }
-
-    // var d_ans = new Map();
-    // ans.forEach(object => {
-    //     for (const key in object) {
-    //         d_ans.set(key, object[key])
-    //     }
-    // })
-
-    // console.log(ans);
-    // var d_ans = [ ...new Set(ans) ];
-    res.status(200).json({ users: d_ans });
+    for( id of ans) d_ans.push(JSON.parse(id));
+    var message = `Successfully found Level ${k} friends of ${req.profile.username}.`;
+    if(!(d_ans.length)) message =  `${req.profile.username} doesn't have any Level ${k} friends. Maximum Level for them is ${Math.floor(currLevel/2)} .`;
+    res.status(200).json({ message:message, users: d_ans });
 };
 
 exports.displayUser = (req, res) => {
